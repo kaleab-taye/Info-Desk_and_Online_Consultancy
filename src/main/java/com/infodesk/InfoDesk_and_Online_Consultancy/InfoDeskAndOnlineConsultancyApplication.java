130e4cdbc05b8cc33977cb163c6be081386e1f4c
kaleab-taye <kaleabtaye@gmail.com>
2022-01-13 14:13:11 +0300

    launching the spring application
    launching the spring application

---
+++ src/main/java/com/infodesk/InfoDesk_and_Online_Consultancy/InfoDeskAndOnlineConsultancyApplication.java
@@ -0,0 +1,13 @@
+package com.infodesk.InfoDesk_and_Online_Consultancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoDeskAndOnlineConsultancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoDeskAndOnlineConsultancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infodesk/InfoDesk_and_Online_Consultancy/InfoDeskAndOnlineConsultancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infodesk.InfoDesk_and_Online_Consultancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoDeskAndOnlineConsultancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
